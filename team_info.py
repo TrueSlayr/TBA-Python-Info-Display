b9af44c8f36b9886752c2e72f16f79c6e9c3a3e3
Rishikesh Desai <minecraft030603@gmail.com>
2020-07-20 03:11:49 -0400

    Update v1.2
    Updated for the 2020 FRC Season. Now displays events attended by the team in 2020, rather than 2019.

--- team_info.py
+++ team_info.py
@@ -4,16 +4,16 @@ from pytba import api as tba
 #End Imports
 
 #Begin API Key Verification
-tba = tbapy.TBA('(tba api key here)')
+tba = tbapy.TBA('tba api key here')
 #End API Key Verification
 
 team_number_provided = input("Input Team Number: ")
 team_number = team_number_provided
 team_number_appended = str('frc') + str(team_number_provided)
 team = tba.team(team_number_appended)
-events = tba.team_events(team_number_appended, 2019)
+events = tba.team_events(team_number_appended, 2020)
 
 if team_number_appended == str("frc") + (team_number_provided): # Check whether string for Team Number is formatted properly
     print(("Team ") + team_number + "'s", "Info:") # Print Title
     print('Team ' + team_number + ' is from %s in %s, %s.' % (team.city, team.state_prov, team.country)) # Print Team Location
-    print('In 2019, Team ' + team_number + ' participated at %d events: %s.' % (len(events), ', '.join(event.event_code for event in events))) # Print Team Events
+    print('In 2020, Team ' + team_number + ' participated at %d events: %s.' % (len(events), ', '.join(event.event_code for event in events))) # Print Team Events
